9de65398369e59ca11e020182b5288e76106d6fa
Madeira Dylan <madeira.dylan@gmail.com>
2024-11-13 10:07:01 +0100

    Version final du carrousel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,69 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Sélection des flèches
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+
+// Dots
+const dots = document.querySelector('.dots');
+
+// Slides
+const bannerImg = document.querySelector('.banner-img');
+const bannerText = document.querySelector('p');
+let slide = 0;
+
+// Ajout des event listeners des fleches
+arrowLeft.addEventListener('click', function () {
+	slide--;
+    updateSlider();
+    console.log(slide = (slide + slides.length) % slides.length);
+	console.log('Flèche gauche cliquée');
+
+});
+
+arrowRight.addEventListener('click', function () {
+	slide++;
+    updateSlider();
+    console.log(slide = (slide + slides.length) % slides.length);
+	console.log('Flèche gauche cliquée');
+
+});
+
+
+// Création des dots
+function initDots() 
+{
+	dots.replaceChildren();
+    for (let i = 0; i < slides.length; i++) 
+	{
+        const dot = document.createElement('div');
+        dot.classList.add('dot'); 
+		
+		if (i === slide)
+			dot.classList.add('dot_selected'); 
+	
+        dots.appendChild(dot);
+    }
+}
+
+//Update le carousel
+function updateSlider() 
+{
+	/* 
+	Tableau utilisé circulairement, modulo sa taille
+	Si slide < 0
+		(-1+4)%4 = 3 donc derniere slide
+	Si slide > slides.length
+		(4+4)%4 = 0 donc premiere slide
+	*/
+	slide = (slide + slides.length) % slides.length;
+
+    bannerImg.src = "assets/images/slideshow/" + slides[slide].image;
+	bannerText.innerHTML = slides[slide].tagLine;
+	initDots();
+}
+
+// initialise les dots au début
+initDots();
